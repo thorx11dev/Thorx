da74154104093c7ad6107d6b45f0eaa13080ccaf
thorx11dev <thorx11dev@users.noreply.github.com>
2026-08-24 22:34:04 -0700

    chore(sync): auto-sync 1 file(s): main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
+import { initPosthog } from "@/lib/posthog";
+
+initPosthog();
 
 createRoot(document.getElementById("root")!).render(
   <ErrorBoundary scope="Application">
